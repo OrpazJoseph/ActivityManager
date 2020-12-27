8d71ad47af79c529296a6e0de248c0020234051e
OrpazJoseph <orpazexe@gmail.com>
2020-12-27 19:37:06 +0200

    Master

---
+++ README.md
@@ -0,0 +1,31 @@
+
+## Activity Manager
+
+Creators: Orpaz Joseph, Dima Khmelev, Yehonatan Negbi, and Or Ganzian
+
+## General info
+This project is a final project at 'Software engineering' course at HIT.
+The Activity Manager is a program to manage the activites of any organization like a municipality or any other institution that offer activities and want to have an organized management.
+The Activity manger support user mode & admin mode.
+	
+## About the project
+Project is created with:
+* Java
+* SQLite
+* Junit
+* MVC architecture
+* Design Patterns: Factory, Singleton
+* Eclipse
+* command-line interface
+	
+## Setup
+To run this project, you have to:
+
+```
+* Place the DB file in the right folder
+* Make sure the activites reports are craeted in a valid path
+
+
+
+```
+
---
+++ src/model/OtherActivity.java
@@ -0,0 +1,78 @@
+package model;
+
+public class OtherActivity extends Activity{
+	
+	
+	// attributes
+	private String guide;
+	private String category;
+	private String ageRange;
+	
+	
+	// C'tor + get\set
+	
+	public OtherActivity(int activityID)
+	{
+		this.activityID=activityID;
+	}
+	
+	
+
+	public String getGuide() {
+		return guide;
+	}
+
+	public void setGuide(String guide) {
+		this.guide = guide;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getAgeRange() {
+		return ageRange;
+	}
+
+	public void setAgeRange(String ageRange) {
+		this.ageRange = ageRange;
+	}
+	
+	public OtherActivity( ) 
+	{
+		super();
+		guide = "non";
+		category = "non";
+		ageRange = "non";
+	}
+	
+	public OtherActivity(int activityID, String name, String location, int currentCapicity, int maxCapicity, double price,
+			String description, String guide,String categorty,String ageRange) 
+	{
+		super(activityID,name, location, currentCapicity,maxCapicity, price, description);
+		this.guide=guide;
+		this.category=categorty;
+		this.ageRange=ageRange;
+	}
+	
+	public OtherActivity(int activityID, String name, double price) 
+	{
+		super(activityID, name, name, activityID, activityID, price, name);
+		this.guide="non";
+		this.category="non";
+		this.ageRange="non";
+	}
+	
+	// methods
+
+	@Override
+	public String toString() {
+		return "Activity ID: "+this.activityID+" | Name: "+this.name+" | Location: "+this.location+" | Price: "+this.price+" | Guide: "+this.guide+" |Age Range: "+this.ageRange+" | Description: "+this.description;
+	}
+	
+
+}
